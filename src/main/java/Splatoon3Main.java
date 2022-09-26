b8d2b397dd4b7c6bcdbddb057ae32ea887d207dc
easyworld <easy_world_118@Hotmail.com>
2022-09-26 22:20:23 +0800

    feat: generate noexes json

--- src/main/java/Splatoon3Main.java
+++ src/main/java/Splatoon3Main.java
@@ -5,6 +5,8 @@ import io.net.NetworkConstants;
 import io.net.SocketConnection;
 import misc.DebuggerHelper;
 
+import java.io.PrintWriter;
+import java.nio.ByteOrder;
 import java.util.Arrays;
 
 public class Splatoon3Main {
@@ -20,9 +22,11 @@ public class Splatoon3Main {
         System.out.println("你输入的ip地址为：" + args[0]);
         System.out.println("开始找地址");
         Debugger debugger = new Debugger(new SocketConnection(DebuggerHelper.buildSocket(args[0], NetworkConstants.DEFAULT_PORT, NetworkConstants.TIMEOUT)));
-        Arrays.stream(debugger.getPids()).filter(pid -> TID == debugger.getTitleId(pid)).findFirst().ifPresent(debugger::attach);
+
         try {
+            Arrays.stream(debugger.getPids()).filter(pid -> TID == debugger.getTitleId(pid)).findFirst().ifPresent(debugger::attach);
             if (debugger.attached()) {
+                debugger.resume();
                 MemoryInfo[] query = debugger.query(0, 10000);
                 long heap1 = 0;
                 for (MemoryInfo memoryInfo : query) {
@@ -32,14 +36,45 @@ public class Splatoon3Main {
                 }
                 if (heap1 == 0) {
                     System.out.println("没找到");
+                    return;
                 }
-                long money1 = heap1 + 0x1092E4;
-                long money2 = heap1 + 0x20D3C4;
-                System.out.printf("金币地址1：0x%x%n金币地址1：0x%x%n", money1, money2);
+                long money1Address = heap1 + 0x1092E4;
+                long money2Address = heap1 + 0x20D3C4;
+                long money1 = debugger.readmem(money1Address, 4, new byte[4]).order(ByteOrder.LITTLE_ENDIAN).getInt() & 0xffffffffL;
+                long money2 = debugger.readmem(money2Address, 4, new byte[4]).order(ByteOrder.LITTLE_ENDIAN).getInt() & 0xffffffffL;
+                if (money1 == money2) {
+                    System.out.println("请核对你的金币数是否为" + money1);
+                    System.out.printf("金币地址1：0x%x%n金币地址1：0x%x%n", money1Address, money2Address);
+                    PrintWriter pw = new PrintWriter("splatoon3_money.json");
+                    pw.println(String.format(JSON, money1Address, money2Address));
+                    pw.close();
+                } else {
+                    System.out.println("情况不妙，数据似乎不对");
+                    return;
+                }
             }
         } finally {
             debugger.close();
         }
-        System.out.println("完事");
+        System.out.println("完事，准备用noexes导入生成的splatoon3_money.json");
     }
+
+    private static final String JSON = "[\n" +
+            "  {\n" +
+            "    \"update\": true,\n" +
+            "    \"locked\": false,\n" +
+            "    \"addr\": \"%s\",\n" +
+            "    \"desc\": \"money1\",\n" +
+            "    \"type\": \"INT\",\n" +
+            "    \"value\": 4700\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"update\": true,\n" +
+            "    \"locked\": false,\n" +
+            "    \"addr\": \"%s\",\n" +
+            "    \"desc\": \"money2\",\n" +
+            "    \"type\": \"INT\",\n" +
+            "    \"value\": 4700\n" +
+            "  }\n" +
+            "]";
 }
